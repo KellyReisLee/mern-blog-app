a8f3e1c27a67bc4871743e3dd9f3137c020899a6
KellyReisLee <kristinaalvares28@gmail.com>
2024-03-06 08:07:35 +0000

    setting react-router-dom

--- mern-blog-complete/src/main.jsx
+++ mern-blog-complete/src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter } from 'react-router-dom'
+import Layout from './components/Layout.jsx'
+import ErrorPage from './pages/ErrorPage.jsx'
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+
+  }
+]
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </React.StrictMode>,
 )
