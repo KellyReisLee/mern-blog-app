067b887260a2dc50eb6e1d33716ae326b30cfdbd
KellyReisLee <kristinaalvares28@gmail.com>
2024-03-06 15:56:25 +0000

    Creating Footer component

--- mern-blog-complete/src/components/Footer.jsx
+++ mern-blog-complete/src/components/Footer.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import classes from './Footer.module.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className={classes.footer}>
+      <ul>
+        <li>
+          <Link to="/posts/categories/:Javascript">Javascript</Link>
+        </li>
+        <li>
+          <Link to="/posts/categories/:CSS">CSS</Link>
+        </li>
+        <li>
+          <Link to="/posts/categories/:HTML">HTML</Link>
+        </li>
+        <li>
+          <Link to="/posts/categories/:Node.js">Node.js</Link>
+        </li>
+        <li>
+          <Link to="/posts/categories/:Bootstrap">Bootstrap</Link>
+        </li>
+        <li>
+          <Link to="/posts/categories/:React.js">React.js</Link>
+        </li>
+        <li>
+          <Link to="/posts/categories/:Tailwind">Tailwind</Link>
+        </li>
+
+      </ul>
+      <div className={classes.copyright}>
+        <small>All Rights Reserved &copy; Copyright, Kelly Reis</small>
+
+      </div>
+    </footer>
   )
 }
 
