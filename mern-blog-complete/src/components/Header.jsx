b72e3240ac75128e4371fc597744f9a0221361d8
KellyReisLee <kristinaalvares28@gmail.com>
2024-03-06 09:30:06 +0000

    create header component

--- mern-blog-complete/src/components/Header.jsx
+++ mern-blog-complete/src/components/Header.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+import Logo from '../assets/blog-1.jpg'
+import { GiHamburgerMenu } from "react-icons/gi";
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav>
+      <div className='container nav__container'>
+        <Link to='/' className='nav__logo'>
+          <img src={Logo} alt='logo-computer' />
+        </Link>
+        <ul className='nav__menu'>
+          <li>
+            <Link to="/profile">Ernest Smith</Link>
+          </li>
+          <li>
+            <Link to="/create">Create Post</Link>
+          </li>
+          <li>
+            <Link to="/authors">Authors</Link>
+          </li>
+          <li>
+            <Link to="/logout">Logout</Link>
+          </li>
+        </ul>
+        {/* Navbar for a mobile */}
+        <button className='nav__toggle-btn'>
+          <GiHamburgerMenu />
+        </button>
+      </div>
+    </nav>
   )
 }
 
--- mern-blog-complete/src/main.jsx
+++ mern-blog-complete/src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/Layout.jsx'
 import ErrorPage from './pages/ErrorPage.jsx'
+import Home from './pages/Home.jsx'
+import PostDetail from './pages/PostDetail.jsx'
+import RegisterPage from './pages/RegisterPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import UserProfile from './pages/UserProfile.jsx'
+import CreatePosts from './pages/CreatePosts.jsx'
+import EditPost from './pages/EditPost.jsx'
+import CategoryPosts from './pages/CategoryPosts.jsx'
+import AuthorPosts from './pages/AuthorPosts.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import Logout from './pages/Logout.jsx'
+
 
 
 const router = createBrowserRouter([
@@ -12,6 +24,19 @@ const router = createBrowserRouter([
     path: "/",
     element: <Layout />,
     errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "posts/:id", element: <PostDetail /> },
+      { path: "register", element: <RegisterPage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "userProfile", element: <UserProfile /> },
+      { path: "create", element: <CreatePosts /> },
+      { path: "posts/categories/:category", element: <CategoryPosts /> },
+      { path: "posts/users/:id", element: <AuthorPosts /> },
+      { path: "myposts/:id", element: <Dashboard /> },
+      { path: "posts/:id/edit", element: <EditPost /> },
+      { path: "logout", element: <Logout /> },
+    ]
 
   }
 ]
