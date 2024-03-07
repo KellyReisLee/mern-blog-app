e35eb2f4a235ecd439df7cd3084f3c0eaab6ccde
KellyReisLee <kristinaalvares28@gmail.com>
2024-03-07 17:26:43 +0000

    Adding some style to ErrorPage

--- mern-blog-complete/src/pages/ErrorPage.jsx
+++ mern-blog-complete/src/pages/ErrorPage.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import classes from './ErrorPage.module.css'
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <section className={classes.errorPg}>
+      <div className='center'>
+        <Link to="/" className='btn'>Go Back to Home</Link>
+        <h2>Page Not Found</h2>
+      </div>
+
+    </section >
   )
 }
 
