8276326b78e4c9e5601b359a2e8b9813cc9656ed
KellyReisLee <kristinaalvares28@gmail.com>
2024-03-10 14:09:37 +0000

    Creating the register page

--- mern-blog-complete/src/pages/RegisterPage.jsx
+++ mern-blog-complete/src/pages/RegisterPage.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import { useState } from 'react'
+import classes from './Register.module.css'
+import { Link } from 'react-router-dom'
 
 const RegisterPage = () => {
+  const [userData, setUserData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  })
+
+  function changeInputHandler(e) {
+    setUserData((prev) => {
+      return { ...prev, [e.target.name]: e.target.value }
+    })
+
+  }
+
   return (
-    <div>RegisterPage</div>
+    <section className={classes.register}>
+      <div className={classes.mainRegister}>
+        <h2>Sign up</h2>
+        <form className={classes.form}>
+          <p>This is an error message</p>
+          <input
+            type='text'
+            placeholder='Username'
+            name='username'
+            value={userData.username}
+            onChange={changeInputHandler}
+            autoFocus
+
+          />
+          {/* email */}
+          <input
+            type='email'
+            placeholder='Email'
+            name='email'
+            value={userData.email}
+            onChange={changeInputHandler}
+
+          />
+          {/* password */}
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={userData.password}
+            onChange={changeInputHandler}
+
+          />
+          {/* confirm password */}
+          <input
+            type='password'
+            placeholder='Confirm Password'
+            name='confirmPassword'
+            value={userData.confirmPassword}
+            onChange={changeInputHandler}
+
+          />
+        </form>
+        <button>Register</button>
+        <small>Already have an account? <Link to="/login">Sign in</Link></small>
+
+      </div>
+
+    </section>
   )
 }
 
