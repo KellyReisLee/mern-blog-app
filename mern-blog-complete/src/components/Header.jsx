4939c2943c6cbbf680476143c408e0048121a50d
KellyReisLee <kristinaalvares28@gmail.com>
2024-03-12 20:22:00 +0000

    Adding some styles to edit page

--- mern-blog-complete/src/components/Header.jsx
+++ mern-blog-complete/src/components/Header.jsx
@@ -1,35 +1,48 @@
-import React from 'react'
+import { useState } from 'react'
 import { Link } from "react-router-dom"
 import Logo from '../assets/logo.jpg'
 import { GiHamburgerMenu } from "react-icons/gi";
-// import { IoMdClose } from "react-icons/io";
+import { IoMdClose } from "react-icons/io";
 import classes from './Header.module.css'
 
 
 const Header = () => {
+
+  const [show, setShow] = useState(window.innerWidth > 800 ? true : false)
+
+  function handleCloseNav() {
+    if (window.innerWidth < 800) {
+      setShow(false)
+    } else {
+      setShow(true)
+    }
+  }
+
   return (
     <nav className={classes.navHeader}>
       <div className={classes.containerHeader}>
-        <Link to='/' className={classes.logoImg}>
+        <Link to='/' className={classes.logoImg} onClick={handleCloseNav}>
           <img src={Logo} alt='logo-computer' />
         </Link>
-        <ul className={classes.navMenu}>
-          <li>
-            <Link to="/profile/rsrss">Ernest Smith</Link>
-          </li>
-          <li>
-            <Link to="/create">Create Post</Link>
-          </li>
-          <li>
-            <Link to="/authors">Authors</Link>
-          </li>
-          <li>
-            <Link to="/logout">Logout</Link>
-          </li>
-        </ul>
+        {show && (
+          <ul className={classes.navMenu}>
+            <li>
+              <Link to="/profile/rsrss" onClick={handleCloseNav}>Ernest Smith</Link>
+            </li>
+            <li>
+              <Link to="/create" onClick={handleCloseNav}>Create Post</Link>
+            </li>
+            <li>
+              <Link to="/authors" onClick={handleCloseNav}>Authors</Link>
+            </li>
+            <li>
+              <Link to="/logout" onClick={handleCloseNav}>Logout</Link>
+            </li>
+          </ul>
+        )}
         {/* Navbar for a mobile */}
-        <button className={`${classes.btnHeader} ${classes.displayNone}`}>
-          <GiHamburgerMenu />
+        <button onClick={() => setShow(!show)} className={`${classes.btnHeader}`}>
+          {show ? <IoMdClose /> : < GiHamburgerMenu />}
         </button>
       </div>
     </nav>
