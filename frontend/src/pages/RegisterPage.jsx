65aacf2925236d8ae59a629dca7a70b1b751c489
KellyReisLee <kristinaalvares28@gmail.com>
2024-03-13 12:12:50 +0000

    Organizing data

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -10,6 +10,57 @@ const RegisterPage = () => {
     confirmPassword: ''
   })
 
+  const isValid = (password) => {
+    // Regular expression for password validation
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/;
+    // Test the password against the regular expression
+    return regex.test(password);
+
+  }
+
+  function validation() {
+    const { username, password, confirmPassword } = userData;
+
+    if (username.length < 4) {
+      alert('The username must contain at least 4 characters.')
+      return false
+    } else if (!isValid(password)) {
+      alert('Password must contain minimum 8 characters, including: 1 special character(! # $ % ?) and 1 capital letter.')
+      return false
+
+    } else if (password !== confirmPassword) {
+      alert('Password and Confirm Password have to match!')
+      return false
+    }
+    return true
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (validation) {
+      try {
+        const { data } = await axios.post("/register", {
+          username, email, password
+        })
+
+        if (data.error) {
+          alert(error)
+        } else {
+          alert('Register Successful. Welcome!')
+          setUserData({})
+
+        }
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+  }
+
+
+
   function changeInputHandler(e) {
     setUserData((prev) => {
       return { ...prev, [e.target.name]: e.target.value }
@@ -21,7 +72,7 @@ const RegisterPage = () => {
     <section className={classes.register}>
       <div className={classes.mainRegister}>
         <h2>Sign Up</h2>
-        <form className={classes.form}>
+        <form onSubmit={handleSubmit} className={classes.form}>
           <p>This is an error message</p>
           <input
             type='text'
