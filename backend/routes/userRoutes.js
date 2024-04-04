5637c47292c7e9ec75ecbbfd8ebb73fbf9d29ad6
KellyReisLee <kristinaalvares28@gmail.com>
2024-04-04 11:24:47 +0100

    adjusting logout logic

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -21,7 +21,7 @@ router.get('/', getAuthors)
 router.post('/change-avatar', changeImgUser)
 router.patch('/:id/edit-user', editUser)
 router.get('/verify/:id/:token', getVerification)
-router.get('/logout', userLogout)
+router.get('/api/users/logout', userLogout)
 router.post('/send-email', userSendEmail)
 router.patch('/change-password/:id/:token', changePassword)
 
